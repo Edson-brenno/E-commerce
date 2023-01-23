f71b93c654f67f3bd1849baea577cb36b869cf2c
brenno <brennorebeiro@gmail.com>
2023-01-23 19:10:16 -0300

    Adiciona model TbCategoriasProdutosFat v0.2.1

--- categoria_produtos/models.py
+++ categoria_produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TbCategoriasProdutosFat(models.Model):
+
+    categoria = models.TextField(max_length=70, null=False, blank=False)
+    descricao = models.TextField(max_length=150, null=False, blank=False)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_criacao = models.DateTimeField(auto_now=True)
+    
